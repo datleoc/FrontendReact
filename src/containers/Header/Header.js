1814619e05e62babf68b2c578783f7ee65b4bb24
Steven Le <uslb98@gmail.com>
2023-08-09 15:34:10 +0700

    #57' git commit -m

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,6 +5,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils';
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
 
@@ -13,7 +14,7 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
 
         return (
             <div className="header-container">
@@ -23,6 +24,9 @@ class Header extends Component {
                 </div>
 
                 <div className="languages">
+                    <span className="welcome">
+                        <FormattedMessage id="homeheader.welcome" />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''}!</span>
                     <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>
                         VN
@@ -47,6 +51,7 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        userInfo: state.user.userInfo
     };
 };
 
