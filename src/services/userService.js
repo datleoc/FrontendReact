8bd960ee212b0b02b9d5994500fda44b2d93bb05
datleoc <116932108+datleoc@users.noreply.github.com>
2023-10-11 14:52:20 +0700

    #92

--- src/services/userService.js
+++ src/services/userService.js
@@ -67,6 +67,18 @@ const postPatientBookAppointment = (data) => {
     return axios.post('/api/patient-book-appointment', data)
 }
 
+const postVerifyBookAppointment = (data) => {
+    return axios.post('/api/verify-book-appointment', data)
+}
+
+const createNewSpecialty = (data) => {
+    return axios.post('/api/create-new-specialty', data)
+}
+
+const getAllSpecialty = () => {
+    return axios.get(`/api/get-specialty`);
+}
+
 export {
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService,
@@ -75,5 +87,6 @@ export {
     saveDetailDoctorService, getDetailInforDoctor,
     saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById,
-    postPatientBookAppointment
+    postPatientBookAppointment, postVerifyBookAppointment,
+    createNewSpecialty, getAllSpecialty
 }
